b01d20dbd6b4b81a5026fbd62717aa2c590d35dc
ChangfengHU <faker_322@outlook.com>
2019-01-19 16:24:11 +0800

    加入sql

--- scrapy/Tencent/Tencent/items.py
+++ scrapy/Tencent/Tencent/items.py
@@ -4,9 +4,13 @@
 #
 # See documentation in:
 # http://doc.scrapy.org/en/latest/topics/items.html
-
+from scrapy.loader import ItemLoader
+from scrapy.loader.processors import MapCompose, TakeFirst, Join
+from Tencent.settings import SQL_DATETIME_FORMAT, SQL_DATE_FORMAT
+from w3lib.html import remove_tags
 import scrapy
-
+import datetime
+import re
 class TencentItem(scrapy.Item):
     #职位名
     positionName = scrapy.Field()
@@ -47,7 +51,6 @@ class ArticleItem(scrapy.Item):
     title = scrapy.Field()
     avatar = scrapy.Field()
     author = scrapy.Field()
-    category = scrapy.Field()
     pub_time = scrapy.Field()
     article_id = scrapy.Field()
     content = scrapy.Field()
@@ -56,3 +59,84 @@ class ArticleItem(scrapy.Item):
 class KugouMusicItem(scrapy.Item):
     category = scrapy.Field()
     url = scrapy.Field()
+
+
+
+
+
+def add_jobbole(value):
+    return value+"-bobby"
+
+
+def date_convert(value):
+    try:
+        create_date = datetime.datetime.strptime(value, "%Y/%m/%d").date()
+    except Exception as e:
+        create_date = datetime.datetime.now().date()
+
+    return create_date
+
+
+def get_nums(value):
+    match_re = re.match(".*?(\d+).*", value)
+    if match_re:
+        nums = int(match_re.group(1))
+    else:
+        nums = 0
+
+    return nums
+
+def return_value(value):
+    return value
+
+
+def remove_comment_tags(value):
+    #去掉tag中提取的评论
+    if "评论" in value:
+        return ""
+    else:
+        return value
+
+class JobBoleArticleItem(scrapy.Item):
+    title = scrapy.Field()
+    create_date = scrapy.Field(
+        input_processor=MapCompose(date_convert),
+    )
+    url = scrapy.Field()
+    url_object_id = scrapy.Field()
+    front_image_url = scrapy.Field(
+        output_processor=MapCompose(return_value)
+    )
+    front_image_path = scrapy.Field()
+    praise_nums = scrapy.Field(
+        input_processor=MapCompose(get_nums)
+    )
+    comment_nums = scrapy.Field(
+        input_processor=MapCompose(get_nums)
+    )
+    fav_nums = scrapy.Field(
+        input_processor=MapCompose(get_nums)
+    )
+    tags = scrapy.Field(
+        input_processor=MapCompose(remove_comment_tags),
+        output_processor=Join(",")
+    )
+    content = scrapy.Field()
+
+    def get_insert_sql(self):
+        insert_sql = """
+            insert into jobbole_article(title, url, create_date, fav_nums, front_image_url, front_image_path,
+            praise_nums, comment_nums, tags)
+            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s) ON DUPLICATE KEY UPDATE fav_nums=fav_nums+1
+        """
+
+        fron_image_url = ""
+        front_image_path = ""
+        # content = remove_tags(self["content"])
+
+        if self["front_image_url"]:
+            fron_image_url = self["front_image_url"][0]
+        params = (self["title"], self["url"], self["create_date"], self["fav_nums"],
+                  fron_image_url, front_image_path, self["praise_nums"], self["comment_nums"],
+                  self["tags"])
+        return insert_sql, params
--- scrapy/Tencent/Tencent/pipelines.py
+++ scrapy/Tencent/Tencent/pipelines.py
@@ -5,6 +5,23 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
+#方案0
+import json
+from scrapy.exporters import JsonItemExporter
+class MinePipeline(object):
+    def __init__(self):
+        self.f = open("data\\jobbole1.json", "w")
+    def process_item(self, item, spider):
+        #dicq = eval(str(item))
+        print(spider.name)
+        temp = {}
+        content = json.dumps(dict(eval(str(item))), ensure_ascii=False)+",\n"
+        self.f.write(content)
+        print(content)
+        return item
+
+    def close_spider(self, spider):
+        self.f.close()
 #方案一
 # import json
 # from scrapy.exporters import JsonItemExporter
@@ -58,7 +75,7 @@ from scrapy.exporters import JsonLinesItemExporter
 class TencentPipeline(object):
     def __init__(self):
         # self.fp = open("D:\\spark-input\\kugouMusic.json", "wb")
-        self.fp = open("data\\kugouMusic.json", "wb")
+        self.fp = open("data\\jobbole.json", "wb")
         self.exporter = JsonLinesItemExporter(self.fp,ensure_ascii=False,encoding="utf-8")
 
     def close_spider(self, spider):
@@ -76,11 +93,12 @@ import os
 import scrapy
 from Tencent.settings import IMAGES_STORE as images_store
 from Tencent.settings import IMAGES_STORE_GIRLS as images_store_girls
+from Tencent.settings import IMAGES_STORE_JOBBOLE as images_store_jobbole
 from scrapy.pipelines.images import ImagesPipeline
-class DouyuPipeline(ImagesPipeline):
+class DouyuImagePipeline(ImagesPipeline):
 
     def get_media_requests(self, item, info):
-        image_link = item['imagelink']
+        image_link = item['front_image_url']
         yield scrapy.Request(image_link)
 
     def item_completed(self, results, item, info):
@@ -92,6 +110,37 @@ class DouyuPipeline(ImagesPipeline):
         os.rename(images_store+ image_path[0], images_store_girls + name + ".jpg")
         return item
 
+class JobboleImagePipeline(ImagesPipeline):
+    def get_media_requests(self, item, info):
+        image_link = item['front_image_url'][0]
+        yield scrapy.Request(image_link)
+
+    def item_completed(self, results, item, info):
+        # print (results)
+        # print ("*" * 30)
+        # 取出results里图片信息中的 图片路径的值
+        name=str(item["title"])
+        image_path = [x["path"] for ok, x in results if ok]
+        os.rename(images_store + image_path[0], images_store_jobbole + name + ".jpg")
+        return item
+
+class JobboleImagePipeline1(object):
+
+    def __init__(self):
+        self.path = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'images')
+        if not os.path.exists(self.path):
+            os.mkdir(self.path)
+
+    def process_item(self, item, spider):
+        category = item['tags']
+        category_path=os.path.join(self.path,category)
+        urls = item['front_image_url']
+        if not os.path.exists(category_path):
+            os.mkdir(category_path)
+        for url in urls:
+            image_name = item['title']+".jpg"
+            request.urlretrieve(url, os.path.join(category_path, image_name))
+        return item
 class BMW5Pipeline(object):
 
     def __init__(self):
@@ -176,5 +225,54 @@ class mysqlPoolPipline(object):
         print("=" * 10 + "error" + "=" * 10)
 
 
+class jobboleMysqlPipeline(object):
+    #采用同步的机制写入mysql
+    def __init__(self):
+        dbparam = {
+            "host": "127.0.0.1",
+            "port": 3306,
+            "user": "root",
+            "password": "123456789",
+            "database": "jianshu",
+            "charset": "utf8"
+        }
+        self.conn = pymysql.connect(**dbparam)
+        self.cursor = self.conn.cursor()
+        self._sql = None
+
+    def process_item(self, item, spider):
+        insert_sql = """
+            insert into jobbole_article(title, url, create_date, fav_nums)
+            VALUES (%s, %s, %s, %s)
+        """
+        self.cursor.execute(insert_sql, (item["title"], item["url"], item["create_date"], item["fav_nums"]))
+        self.conn.commit()
 
 
+
+class jobbolemysqlPoolPipline(object):
+    def __init__(self):
+        dbparam = {
+            "host": "127.0.0.1",
+            "port": 3306,
+            "user": "root",
+            "password": "123456789",
+            "database": "jianshu",
+            "charset": "utf8",
+            'cursorclass':cursors.DictCursor
+        }
+        self.dbpool=adbapi.ConnectionPool('pymysql',**dbparam)
+    def handle_error(self,error,item,spider):
+        print("="*10+"error"+"="*10)
+        print(error)
+        print("=" * 10 + "error" + "=" * 10)
+    def process_item(self, item, spider):
+        # 使用twisted将mysql插入变成异步执行
+        query = self.dbpool.runInteraction(self.do_insert, item)
+        query.addErrback(self.handle_error, item, spider)  # 处理异常
+    def do_insert(self, cursor, item):
+        # 执行具体的插入
+        # 根据不同的item 构建不同的sql语句并插入到mysql中
+        insert_sql, params = item.get_insert_sql()
+        print(insert_sql, params)
+        cursor.execute(insert_sql, params)
